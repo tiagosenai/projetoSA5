3442deb1b56eceb63f791a2f9eca2841c5de5531
Tiago Caceraghi dos Santos <tiago.caceraghi@edu.sc.senai.br>
2023-02-23 18:48:41 -0300

    Versão 0.0.1 - Projeto Inicial

---
+++ src/test/java/br/senai/projetoSA5/ProjetoSa5ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.senai.projetoSA5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoSa5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
